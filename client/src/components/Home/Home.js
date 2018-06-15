bb5cdb50d68393b053ab3f94374acb13a65a4998
lyonx <chrisb000@gmail.com>
2018-06-15 13:47:29 -0700

    mobile responsive margins and main content height

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -10,11 +10,11 @@ class Home extends Component {
             <div className="container">
                <div className="row">
                   <div className="col-md">
-                     <div class="jumbotron">
-                        <h1 className="display-4">Hello, world!</h1>
-                        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                     <div class="jumbotron" id="about">
+                        <h1 className="display-4">About BrightSide</h1>
+                        <p className="lead">We speciallize in providing fair and compassionate Supervised Visitation Monitoring.</p>
                         <hr className="my-4"/>
-                           <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                           <p>Here is some other important statement.</p>
                            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
                         </div>
                      </div>
--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -7,47 +7,12 @@ class Landing extends Component {
 
       return (
          <div>
-            {/* <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-               <ol className="carousel-indicators">
-                  <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-                  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-               </ol>
-               <div className="carousel-inner">
-                  <div className="carousel-item active">
-                     <img className="d-block w-100" src="http://via.placeholder.com/800x400" alt="First slide" />
-                  </div>
-                  <div className="carousel-item">
-                     <img className="d-block w-100" src="http://via.placeholder.com/800x400" alt="Second slide" />
-                  </div>
-                  <div className="carousel-item">
-                     <img className="d-block w-100" src="http://via.placeholder.com/800x400" alt="Third slide" />
-                  </div>
-               </div>
-               <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span className="sr-only">Previous</span>
-               </a>
-               <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span className="sr-only">Next</span>
-               </a>
-            </div> */}
             <div className="hero-image">
                <div className="hero-text">
                   <h1 class="display-4">Welcome to BrightSide Visitation!</h1>
-
+                  <a className="btn btn-dark" href="/home" role="button">Learn more</a>
                </div>
             </div>
-            {/* <img src="http://via.placeholder.com/1200x800" class="img-fluid" alt="Responsive image" /> */}
-            {/* <div className="container-fluid">
-               <div className="row">
-                  <div className="col-md">
-
-                  </div>
-               </div>
-            </div>
-         </div> */}
          </div>
       )
    }
